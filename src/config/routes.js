ada5109785c93403f24cdb302abed86135287d51
joaoh <joao_camargo10@hotmail.com>
2019-12-27 18:59:54 -0300

    Mapping the routes

---
+++ src/config/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+module.exports = function (server) {
+
+    const router = express.Router()
+    server.use('/api', router)
+
+    const BillinCycle = require('../api/billingCycle/billingCycleService')
+    BillinCycle.register(router, '/billingCycles')
+}
